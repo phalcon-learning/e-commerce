993e64073ebd2f4de0e7c48bbe3878218c095191
Dicky Indra Asmara <dickyindraofficial@gmail.com>
2018-06-29 23:21:20 +0700

    auto indent

--- app/migrations/1.0.0/User.php
+++ app/migrations/1.0.0/User.php
@@ -7,48 +7,48 @@ use Phalcon\Mvc\Model\Migration;
 
 class UserMigration_100 extends Migration {
   public function up()
-    {
-        $this->morphTable(
-          'user',
-          [
-            'columns' => [
-                new Column(
-                    'id',
-                    [
-                        'type'          => Column::TYPE_INTEGER,
-                        'size'          => 10,
-                        'unsigned'      => true,
-                        'notNull'       => true,
-                        'autoIncrement' => true,
-                        'first'         => true,
-                    ]
-                ),
-                new Column(
-                    'username',
-                    [
-                        'type'          => Column::TYPE_VARCHAR,
-                        'size'          => 25,
-                        'notNull'       => true,
-                    ]
-                ),
-                new Column(
-                    'password',
-                    [
-                        'type'          => Column::TYPE_VARCHAR,
-                        'size'          => 100,
-                        'notNull'       => true,
-                    ]
-                )
-            ],
-            'indexes' => [
-                new Index(
-                    'PRIMARY',
-                    [
-                        'id',
-                    ]
-                ),
-            ],
-          ]
-        );
+  {
+    $this->morphTable(
+      'user',
+      [
+        'columns' => [
+          new Column(
+            'id',
+            [
+              'type'          => Column::TYPE_INTEGER,
+              'size'          => 10,
+              'unsigned'      => true,
+              'notNull'       => true,
+              'autoIncrement' => true,
+              'first'         => true,
+            ]
+          ),
+          new Column(
+            'username',
+            [
+              'type'          => Column::TYPE_VARCHAR,
+              'size'          => 25,
+              'notNull'       => true,
+            ]
+          ),
+          new Column(
+            'password',
+            [
+              'type'          => Column::TYPE_VARCHAR,
+              'size'          => 100,
+              'notNull'       => true,
+            ]
+            )
+          ],
+          'indexes' => [
+            new Index(
+              'PRIMARY',
+              [
+                'id',
+              ]
+            ),
+          ],
+        ]
+      );
     }
-}
+  }
