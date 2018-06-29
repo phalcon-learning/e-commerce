f1027fdb95c035811bb2a51443d97ee9aa6b9fa6
Dicky Indra Asmara <dickyindraofficial@gmail.com>
2018-06-29 23:15:46 +0700

    add user migration - v1.0.0

---
+++ app/migrations/1.0.0/User.php
@@ -0,0 +1,54 @@
+<?php
+
+use Phalcon\Db\Column as Column;
+use Phalcon\Db\Index as Index;
+use Phalcon\Db\Reference as Reference;
+use Phalcon\Mvc\Model\Migration;
+
+class UserMigration_100 extends Migration {
+  public function up()
+    {
+        $this->morphTable(
+          'user',
+          [
+            'columns' => [
+                new Column(
+                    'id',
+                    [
+                        'type'          => Column::TYPE_INTEGER,
+                        'size'          => 10,
+                        'unsigned'      => true,
+                        'notNull'       => true,
+                        'autoIncrement' => true,
+                        'first'         => true,
+                    ]
+                ),
+                new Column(
+                    'username',
+                    [
+                        'type'          => Column::TYPE_VARCHAR,
+                        'size'          => 25,
+                        'notNull'       => true,
+                    ]
+                ),
+                new Column(
+                    'password',
+                    [
+                        'type'          => Column::TYPE_VARCHAR,
+                        'size'          => 100,
+                        'notNull'       => true,
+                    ]
+                )
+            ],
+            'indexes' => [
+                new Index(
+                    'PRIMARY',
+                    [
+                        'id',
+                    ]
+                ),
+            ],
+          ]
+        );
+    }
+}
