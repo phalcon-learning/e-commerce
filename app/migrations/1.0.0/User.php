10fa0006a4311bee498feb2e2e93d995f506ae44
Dicky Indra Asmara <dickyindraofficial@gmail.com>
2018-06-29 23:37:02 +0700

    fix php indent

--- app/migrations/1.0.0/User.php
+++ app/migrations/1.0.0/User.php
@@ -1,54 +1,66 @@
 <?php
 
-use Phalcon\Db\Column as Column;
-use Phalcon\Db\Index as Index;
-use Phalcon\Db\Reference as Reference;
+use Phalcon\Db\Column;
+use Phalcon\Db\Index;
+use Phalcon\Db\Reference;
 use Phalcon\Mvc\Model\Migration;
 
-class UserMigration_100 extends Migration {
-  public function up()
-  {
-    $this->morphTable(
-      'user',
-      [
-        'columns' => [
-          new Column(
-            'id',
-            [
-              'type'          => Column::TYPE_INTEGER,
-              'size'          => 10,
-              'unsigned'      => true,
-              'notNull'       => true,
-              'autoIncrement' => true,
-              'first'         => true,
+class UserMigration_100 extends Migration
+{
+    public function morph()
+    {
+        $this->morphTable('user', [
+                'columns' => [
+                    new Column(
+                        'id',
+                        [
+                            'type' => Column::TYPE_INTEGER,
+                            'unsigned' => true,
+                            'notNull' => true,
+                            'autoIncrement' => true,
+                            'size' => 10,
+                            'first' => true
+                        ]
+                    ),
+                    new Column(
+                        'username',
+                        [
+                            'type' => Column::TYPE_VARCHAR,
+                            'notNull' => true,
+                            'size' => 25,
+                            'after' => 'id'
+                        ]
+                    ),
+                    new Column(
+                        'password',
+                        [
+                            'type' => Column::TYPE_VARCHAR,
+                            'notNull' => true,
+                            'size' => 100,
+                            'after' => 'username'
+                        ]
+                    )
+                ],
+                'indexes' => [
+                    new Index('PRIMARY', ['id'], 'PRIMARY')
+                ],
+                'options' => [
+                    'TABLE_TYPE' => 'BASE TABLE',
+                    'AUTO_INCREMENT' => '1',
+                    'ENGINE' => 'InnoDB',
+                    'TABLE_COLLATION' => 'latin1_swedish_ci'
+                ],
             ]
-          ),
-          new Column(
-            'username',
-            [
-              'type'          => Column::TYPE_VARCHAR,
-              'size'          => 25,
-              'notNull'       => true,
-            ]
-          ),
-          new Column(
-            'password',
-            [
-              'type'          => Column::TYPE_VARCHAR,
-              'size'          => 100,
-              'notNull'       => true,
-            ]
-            )
-          ],
-          'indexes' => [
-            new Index(
-              'PRIMARY',
-              [
-                'id',
-              ]
-            ),
-          ],
-        ]
-      );
+        );
     }
-  }
+
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+
+    }
+}
